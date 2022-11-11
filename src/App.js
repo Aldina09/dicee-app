34d56fc70567a83f194cf8e481e9389489b88c9a
Asus <aldinae@bk.ru>
2022-11-11 17:59:09 +0600

    Initialized react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [leftDiceNumber, setLeftDiceNumber] = useState(2);
+  const [rightDiceNumber, setRightDiceNumber] = useState(3);
+
+  const onButtonClicked = () =>{
+    console.log("Button clicked");
+    setLeftDiceNumber(Math.floor(Math.random()*6)+1);
+    setRightDiceNumber(Math.floor(Math.random()*5)+1);
+    console.log(leftDiceNumber, rightDiceNumber);
+
+  }
+  const onDefaultButtonClicked1 = () => {
+    console.log('Default button clicked1');
+  }
+  const onDefaultButtonClicked2 = () => {
+    console.log('Default button clicked2');
+    console.log('Default button clicked2');
+  }
+  const onDefaultButtonClicked3 = () => {
+    console.log('Default button clicked3');
+    console.log('Default button clicked3');
+    console.log('Default button clicked3');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Dicee</header>
+      <main>
+        <button onClick={onButtonClicked}><img src={require(`./assets/dice${leftDiceNumber}.png`)}/></button>
+        <button onClick={onButtonClicked}><img src={require(`./assets/dice${rightDiceNumber}.png`)}/></button>
+      </main>
+      <DefaultButton func={onDefaultButtonClicked1} text= '123'/>
+      <DefaultButton func={onDefaultButtonClicked2} text= 'Hello World'/>
+      <DefaultButton func={onDefaultButtonClicked3} text= 'Qwer'/>
     </div>
   );
 }
 
 export default App;
+
+function DefaultButton({
+  text,
+  func
+}) {
+  return <button onClick={func} class='default_button'>Press Me {text}</button>
+}
